7cba56921e9f50e811b2f4d11c90724bd0bd23b7
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-08-31 16:48:44 +0530

    Set up Express and MongoDB with Chat model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./model/chat.js"); 
 
 
 app.set("views", path.join(__dirname, "views"));
@@ -17,6 +18,17 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 
 }
+
+let chat1 = new Chat({
+    from: "Abhi",
+    to: "Dnyandip",
+    msg:"Have you done your work",
+    created_at: Date()
+});
+
+chat1.save().then((res) => {
+    console.log(res);
+});
 
 app.get("/", (req, res) => {
     res.send("Server is working");
