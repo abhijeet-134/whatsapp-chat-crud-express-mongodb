086666c6d76e730675284d224feb7759f268cc0b
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-09-08 17:15:44 +0530

    add new and create chat routes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Chat = require("./model/chat.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended : true}));
 
 
 main().then(() => {
@@ -23,10 +24,32 @@ async function main() {
 // Index Route 
 app.get("/chats", async  (req, res) => {
     let chats = await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs", {chats});
 });
 
+// New Route 
+app.get("/chats/new", (req, res) =>  {
+    res.render("new.ejs");
+})
+
+
+// Create Route 
+app.post("/chats", (req, res) => {
+    let {from, to, msg} = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date()
+    });
+    newChat.save().then((res) => {
+        console.log("Chat was saved..");
+    }).catch((err) => {
+        console.log(err);
+    })
+    res.redirect("/chats");
+})
 
 app.get("/", (req, res) => {
     res.send("Server is working");
