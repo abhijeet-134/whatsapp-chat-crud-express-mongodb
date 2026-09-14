303c65636a61af5038bd0deed1022358182ade98
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-09-14 17:49:17 +0530

    Add DELETE/ Route

--- index.js
+++ index.js
@@ -74,6 +74,15 @@ app.put("/chats/:id", async (req, res) => {
     res.redirect("/chats");
 })
 
+
+// Delete route 
+app.delete("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
+    console.log("Chat is deleted...");
+    res.redirect("/chats");
+})
+
 app.get("/", (req, res) => {
     res.send("Server is working");
 })
