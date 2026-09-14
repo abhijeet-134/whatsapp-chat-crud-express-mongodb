6e839f9aeda390f9c33a4608de4803900223235e
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-09-14 17:22:22 +0530

    Add Update Route

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./model/chat.js"); 
+const methodOverride = require("method-override");
+
 
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 
 main().then(() => {
@@ -59,6 +62,17 @@ app.get("/chats/:id/edit", async (req, res) => {
     res.render("edit.ejs", {chat});
 })
 
+
+// Update route 
+app.put("/chats/:id", async (req, res) => {
+    let {id} = req.params;
+    let { msg:newMsg } = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id,
+        {msg: newMsg},
+        {runValidators: true, new:true}
+    );
+    res.redirect("/chats");
+})
 
 app.get("/", (req, res) => {
     res.send("Server is working");
